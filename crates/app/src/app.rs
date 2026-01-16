736d20b63b16a4c09d6c5b33232d24b3586a4cb5
Zachbishop8104 <zachbishop456@outlook.com>
2026-01-16 10:18:10 -0500

    removed some unused code

--- crates/app/src/app.rs
+++ crates/app/src/app.rs
@@ -54,7 +54,6 @@ impl eframe::App for App {
                         self.video_context.toggle_playback();
                     }
                 }
-                _ => {}
             }
         }
 
